9cb665421e2d334fcf68144bed4c1cca0d2e5a9b
Ramrap <rahul220.iit@gmail.com>
2021-02-23 19:19:04 +0530

    code updated

--- c.cpp
+++ c.cpp
@@ -7,103 +7,99 @@
 using namespace cv;
 using namespace std;
 
-
-
-int cnt=0;
+int cnt = 0;
 vector<Point2f> pts_src;
 
-void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+void CallBackFunc(int event, int x, int y, int flags, void *userdata)
 
-{   
-     if  ( event == EVENT_LBUTTONDOWN )
-     {
+{
+    if (event == EVENT_LBUTTONDOWN)
+    {
         cnt++;
-        pts_src.push_back(Point2f(x,y));
-          cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" <<cnt << endl;
-     }
-    
+        pts_src.push_back(Point2f(x, y));
+        cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+    }
 }
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
- //open the video file for reading
- Mat img = imread("traffic.jpg");
+    //open the video file for reading
+    if (argc <= 1)
+    {
+        cout << "enter argument as image name also \n ./video \"imageName\" ";
+    }
 
-     //if fail to read the image
-     if ( img.empty() ) 
-     { 
-          cout << "Error loading the image" << endl;
-          return -1; 
-     }
+    string image_name = argv[1];
+    image_name += ".jpg";
+    Mat img_rgb = imread(image_name);
 
-     //Create a window
-     namedWindow("My Window", 1);
-
-     //set the callback function for any mouse event
-     
-     setMouseCallback("My Window", CallBackFunc, NULL);
-     
-     //show the image
-     while(cnt<4){
-
-     imshow("My Window", img);
-      waitKey(50);
-     
-     }
-
-
-     
+    //if fail to read the image
+    if (img_rgb.empty())
+    {
+        cout << "Error loading the image" << endl;
+        return -1;
+    }
+    Mat img;
     
-   for(int i=0;i<pts_src.size();i++){
-       cout<<"hellyeah"<<" "<<i<<endl;
-       cout<<pts_src[i].x<<" "<<pts_src[i].y<<" ";
-   }
-   cout<<endl;
 
+    //Create a window
+    namedWindow("My Window", 1);
 
+    //set the callback function for any mouse event
+
+    setMouseCallback("My Window", CallBackFunc, NULL);
+
+    //show the image
+    while (cnt < 4)
+    {
+
+        imshow("My Window", img);
+        waitKey(50);
+    }
+
+    for (int i = 0; i < pts_src.size(); i++)
+    {
+        cout << "hellyeah"
+             << " " << i << endl;
+        cout << pts_src[i].x << " " << pts_src[i].y << " ";
+    }
+    cout << endl;
 
     // Read destination image.
-    Mat im_dst = imread("empty.jpg");
+    Mat im_dst = imread(image_name);
     // Four corners of the book in destination image.
     vector<Point2f> pts_dst;
-    pts_dst.push_back(Point2f(472,52));
-    pts_dst.push_back(Point2f(472,830));
-    pts_dst.push_back(Point2f(800,830));
-    pts_dst.push_back(Point2f(800,52));
+    pts_dst.push_back(Point2f(472, 52));
+    pts_dst.push_back(Point2f(472, 830));
+    pts_dst.push_back(Point2f(800, 830));
+    pts_dst.push_back(Point2f(800, 52));
 
-     cout<<"hello World 3";
+    cout << "hello World 3";
 
     // Calculate Homography
     Mat h = findHomography(pts_src, pts_dst);
 
     // Output image
-    Mat im_out,cropped_img;
+    Mat im_out, cropped_img;
     // Warp source image to destination based on homography
-    cout<<im_dst.size()<<endl;
+    cout << im_dst.size() << endl;
     warpPerspective(img, im_out, h, im_dst.size());
 
     // Display images
     imshow("Source Image", img);
-    imshow("Destination Image", im_dst);
+
     imshow("Warped Source Image", im_out);
 
-    
-    
+    Rect crop_region(472, 52, 800 - 472, 830 - 52);
+    cropped_img = im_out(crop_region);
 
-     Rect crop_region(472,52,800-472,830-52);
-     cropped_img=im_out(crop_region);
+    imshow("cropped image", cropped_img);
 
-     imshow("cropped image", cropped_img);
-     imwrite("./WrappedImage.jpg", im_out); 
-     imwrite("./WrappedCroppedImage.jpg", cropped_img); 
-
-  
-   
-    
+    //for saving image Add desired path according to need
+    imwrite("./WrappedImage.jpg", im_out);
+    imwrite("./WrappedCroppedImage.jpg", cropped_img);
 
     waitKey(0);
 
- return 0;
-
+    return 0;
 }
