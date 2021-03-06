73088a5afa171e68d6fd5530e774f2ed312c5831
satwik621 <78396999+satwik621@users.noreply.github.com>
2021-03-06 19:08:25 +0530

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,103 @@
+//Uncomment the following line if you are compiling this code in Visual Studio
+//#include "stdafx.h"
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int cnt = 0;
+vector<Point2f> pts_src;
+
+
+//for capturing mouse click
+void CallBackFunc(int event, int x, int y, int flags, void *userdata)
+
+{
+    if (event == EVENT_LBUTTONDOWN)
+    {
+        cnt++;
+        pts_src.push_back(Point2f(x, y));
+        cout << "Point Selected :" << x << ", " << y << ")" << endl;
+    }
+}
+void selectpoints(int cnt){
+    Mat img = imread("traffic.jpg");
+
+       //Create a window
+    namedWindow("My Window", 1);
+
+    //set the callback function for any mouse event
+
+    setMouseCallback("My Window", CallBackFunc, NULL);
+
+    //show the image
+    while (cnt < 4)
+    {
+
+        imshow("My Window", img);
+        waitKey(50);
+    }
+}
+Mat cropframe(Mat img, Mat h){
+     // Output image
+    Mat im_out, cropped_img;
+    // Warp source image to destination based on homography
+    warpPerspective(img, im_out, h, img.size());
+
+    Rect crop_region(472, 52, 800 - 472, 830 - 52);
+    cropped_img = im_out(crop_region);
+    return cropped_img;
+}
+
+int main(int argc, char *argv[])
+{
+    //open the video file for reading
+    if (argc <= 1)
+    {
+        cout << "enter argument as image name also \n ./main \"VideoName\" ";
+    }
+    string video_name = argv[1];
+    video_name += ".mp4";
+    VideoCapture vid(video_name);
+
+    //if fail to read the image
+    if (vid.isOpened()==false)
+    {
+        cout << "Error loading the video" << endl;
+        return -1;
+    }
+    // selecting the points on traffic.jpg
+    selectpoints(cnt);
+
+    // Four corners of the book in destination image.
+    vector<Point2f> pts_dst;
+    pts_dst.push_back(Point2f(472, 52));
+    pts_dst.push_back(Point2f(472, 830));
+    pts_dst.push_back(Point2f(800, 830));
+    pts_dst.push_back(Point2f(800, 52));
+
+    // Calculate Homography
+    Mat h = findHomography(pts_src, pts_dst);
+
+    //Finding the empty frame
+    Mat empty =cropframe(imread("empty.jpg"),h);
+
+    //Capturing frames from the video and finding absdiff
+    Mat frame,prev,dst,cropped;
+    int i = 1;
+    while (i<200){
+        vid.set(CAP_PROP_POS_FRAMES, i);
+        vid >> frame;
+        prev=cropped;
+        cropped = cropframe(frame,h);
+        absdiff(cropped,empty,dst);
+        imshow ("image",dst);
+        i=i+3;
+    }
+   
+    waitKey(0);
+
+    return 0;
+}
