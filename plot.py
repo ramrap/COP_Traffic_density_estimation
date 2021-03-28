fee65a20e6356f540fca6aac5173e276c766e373
Satwik Jain <78396999+satwik621@users.noreply.github.com>
2021-03-28 13:33:06 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -7,6 +7,7 @@ x,y,z = np.loadtxt('data.csv',unpack=True,delimiter=',')
 
 plt.plot(x,y)
 plt.plot(x,z)
+plt.legend(['Queue Density','Dynamic Density'])
 plt.axvline(x=1480, color = 'g', linestyle = 'dashed')
 plt.axvline(x=2850, color = 'r', linestyle = 'dashed')
 plt.axvline(x=4250, color = 'g', linestyle = 'dashed')
